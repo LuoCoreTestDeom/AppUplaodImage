ed8af73e5a0da8e0cc7517fe45d6eb19891cddf8
LuoCore <LuoCore@hotmail.com>
2022-09-07 21:09:50 +0800

    123

--- AppUplaodImage/MainActivity.cs
+++ AppUplaodImage/MainActivity.cs
@@ -39,6 +39,7 @@ namespace AppUplaodImage
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+         
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
